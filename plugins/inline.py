722c8865b1baec6667caad717cb07369660a22af
TeenFire <93464465+TeenFire@users.noreply.github.com>
2023-02-24 13:42:50 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -26,14 +26,14 @@ async def answer(bot, query):
     if not await inline_users(query):
         await query.answer(results=[],
                            cache_time=0,
-                           switch_pm_text='You Are Not Premium User',
+                           switch_pm_text='You Are Not A Premium User',
                            switch_pm_parameter="hehe")
         return
 
     if AUTH_CHANNEL and not await is_subscribed(bot, query):
         await query.answer(results=[],
                            cache_time=0,
-                           switch_pm_text='You have to subscribe my channel to use the bot',
+                           switch_pm_text='You have to Join my channel to use Me',
                            switch_pm_parameter="subscribe")
         return
 
